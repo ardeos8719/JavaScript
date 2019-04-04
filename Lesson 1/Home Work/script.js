f60b0de3bfef421b634fe9eb2ca9443dbfbae3f9
ardeos8719 <ardeos87@gmail.com>
2019-04-04 18:20:17 +0300

    Home Work js

---
+++ Lesson 1/Home Work/script.js
@@ -0,0 +1,61 @@
+// ===-----------------------> 1 task
+
+// ===---> 1 variant
+
+var secOneHour = 3600;
+
+var result = (secOneHour * 3);
+
+console.log((result) + ' секунд в 3-х часах');
+
+
+// ===---> 2 variant
+
+var secOneHour = 3600;
+
+
+console.log((secOneHour * 3) + ' секунд в 3-х часах');
+
+// ===---> 3 variant
+
+var secOneMinute = 60;
+var minutOneHour = 60;
+var numberOfHours = 3;
+var result = secOneMinute * minutOneHour * numberOfHours
+
+console.log((result) + ' секунд в 3-х часах');
+
+// ===-------------------------------------------------> 2 task
+
+var num = 1;
+var num = ((num + 12 - 14) * 5) / 7 + 1 - 1;
+
+alert(num);
+
+// ===-------------------------------------------------> 3 task
+
+var a = 10;
+var b = 2;
+var c = 5;
+
+var result2 = a + b + c;
+
+alert(result2);
+
+// ===-------------------------------------------------> 4 task
+
+var a = 17;
+var b = 10;
+
+var c = a - b;
+
+var d = 7;
+
+var result3 = c + d;
+alert(result3);
+
+// ===-------------------------------------------------> 5 task
+
+var name = 'Игорь';
+
+alert('Привет, ' + name + '!');
---
+++ Lesson 1/script.js
@@ -0,0 +1,100 @@
+var COLOR_RED = "#F00";
+
+
+var flower = 'rose';
+
+flower = 'red rose';
+
+var user = 'Oleg', age = 25, message = 'Hello';
+
+var emptyvar;
+
+emptyvar = flower;
+
+
+// Числовое
+var number = 123;
+
+
+//Infinity
+console.log(1/0);
+
+//Nan
+console.log('нечисло' + 5);
+
+//
+
+var str = "<div class=\"\">;</div>";
+
+console.log(str);
+
+
+// true false
+var checked = true;
+
+// null
+
+var age = null;
+
+//undefined
+
+var x;
+
+console.log(x);
+
+
+var user = { name: 'Vasya', age: 25};
+
+console.log(typeof x);  
+
+// операнд - то, к чему применяется оператор.
+// 5 * 2 - операнды
+// унарные операторы - операторы которые применяются к одному операнду
+
+var x = 1;
+
+x = -x;
+
+// Бинарные операторы - применяется к двум операндам
+
+var x = 1; y = 2
+
+console.log( y - x );
+
+// Сложение строк или бинарный плюс +
+
+var a = 'имя' + 'собаки';
+
+console.log(a);
+
+console.log('1' + 2);
+
+console.log( 2 - '1');
+
+var apples = '2';
+var orange = '3';
+
+console.log( +apples + +orange );
+
+
+console.log( 5 % 2);
+
+//Инкремент и декремент
+
+var number = 100;
+
+number++;
+number--;
+console.log(number);
+
+number+= 2;
+
+console.log(number);
+
+//number = number * 2
+number*=2; //204
+console.log(number);
+
+var a = (5, 6);
+
+console.log(a);
